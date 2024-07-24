7d9368e6bf6c558f0aa9d79ee71d62e0d83ffeb1
flexie0o0 <wabipie0o0@gmail.com>
2024-07-24 23:20:42 +0545

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+The following are the stock **closing price** and **volume** of Google
+""")
+
+#define the ticker symbo
+tickerSymbol = 'GOOGL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+#Open  High  Low Close  Volume  Dividends  Stock Splits
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
+
+
